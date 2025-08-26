8d2bebe2915d66243490605409172b094e72654b
Jakub Gaik <jakub.gaik@gmail.com>
2025-08-26 17:21:35 +0200

    add control button and use icons

--- src/components/controls.tsx
+++ src/components/controls.tsx
@@ -1,9 +1,15 @@
-import { Button } from "@yamori-design/react-components";
 import {
   useLocalStorage,
   useSearchParams,
 } from "@yamori-shared/react-utilities";
 import { LocalStorageKeys } from "../constants";
+import {
+  DownloadIcon,
+  PrintIcon,
+  SaveIcon,
+  ShareIcon,
+} from "@yamori-design/icons";
+import { ControlButton } from "./control-button";
 
 type ControlsProps = {
   onDownload: () => void;
@@ -15,27 +21,41 @@ export const Controls: React.FC<ControlsProps> = ({ onDownload }) => {
     LocalStorageKeys.Saved
   );
 
+  const isCollapsed = true; // TODO
+
   return (
     <div className="controls">
-      <Button
+      <ControlButton
+        icon={<ShareIcon />}
+        label="Share"
         onClick={() =>
           navigator.share({
             url: window.location.href,
           })
         }
-      >
-        Share
-      </Button>
-      <Button onClick={() => window.print()}>Print</Button>
-      <Button onClick={onDownload}>Download</Button>
-      <Button
+        collapsed={isCollapsed}
+      />
+      <ControlButton
+        icon={<PrintIcon />}
+        label="Print"
+        onClick={() => window.print()}
+        collapsed={isCollapsed}
+      />
+      <ControlButton
+        icon={<DownloadIcon />}
+        label="Download"
+        onClick={onDownload}
+        collapsed={isCollapsed}
+      />
+      <ControlButton
+        icon={<SaveIcon />}
+        label="Save"
         onClick={() =>
           setSavedTexts([...(savedTexts ?? []), searchParams.text!])
         }
         disabled={!searchParams.text || savedTexts?.includes(searchParams.text)}
-      >
-        Save
-      </Button>
+        collapsed={isCollapsed}
+      />
     </div>
   );
 };
